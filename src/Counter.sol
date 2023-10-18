694f035928e91a83c4a3d7775375759ba7ea8441
Vadim91200 <Kamenets@hotmail.Fr>
2023-10-18 15:47:10 +0200

    Project init

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,48 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
-contract Counter {
-    uint256 public number;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract MyToken is ERC20, ERC20Permit {
+    mapping(address => bool) public allowedUsers;
+    mapping(address => uint8) public userTier;
+
+    modifier onlyAllowedUsers() {
+        require(allowedUsers[msg.sender], "User not allowed");
+        _;
+    }
+    constructor() ERC20("Jian", "JAN") ERC20Permit("Jian") {
+        _mint(msg.sender, 1000000 * 10 ** uint256(decimals()));
     }
 
-    function increment() public {
-        number++;
+    function addToAllowlist(address user, uint8 tier) external onlyOwner {
+        allowedUsers[user] = true;
+        userTier[user] = tier;
+    }
+
+    function getToken(uint256 amount) external payable {
+        require(msg.value >= amount, "Insufficient ETH sent");
+        _mint(msg.sender, amount);
+        _transfer(address(this), msg.sender, amount);
+    }
+
+    function multiLevelDistribution(address[] memory recipients, uint256[] memory amounts, uint8 tier) external onlyOwner {
+        require(recipients.length == amounts.length, "Arrays length mismatch");
+
+        for (uint256 i = 0; i < recipients.length; i++) {
+            require(userTier[recipients[i]] == tier, "Recipient not in specified tier");
+            _mint(recipients[i], amounts[i]);
+        }
+    }
+    
+    function airDrop(address[] memory recipients, uint256 amount) external onlyOwner {
+        for (uint256 i = 0; i < recipients.length; i++) {
+            _mint(recipients[i], amount);
+        }
+    }
+
+    function mintAndSend(address recipient, uint256 amount) external onlyOwner {
+        _mint(recipient, amount);
     }
 }
