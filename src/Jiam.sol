b24731afd1e1dcdca436761477ededf8163e3782
Vadim91200 <kamenets@hotmail.fr>
2023-10-18 16:19:00 +0200

    Deployment successful

--- src/Jiam.sol
+++ src/Jiam.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract MyToken is ERC20, ERC20Permit {
+contract Jiam is ERC20, ERC20Permit {
     mapping(address => bool) public allowedUsers;
     mapping(address => uint8) public userTier;
 
@@ -13,10 +13,11 @@ contract MyToken is ERC20, ERC20Permit {
         _;
     }
     constructor() ERC20("Jian", "JAN") ERC20Permit("Jian") {
+        allowedUsers[msg.sender]= true;
         _mint(msg.sender, 1000000 * 10 ** uint256(decimals()));
     }
 
-    function addToAllowlist(address user, uint8 tier) external onlyOwner {
+    function addToAllowlist(address user, uint8 tier) external onlyAllowedUsers {
         allowedUsers[user] = true;
         userTier[user] = tier;
     }
@@ -27,7 +28,7 @@ contract MyToken is ERC20, ERC20Permit {
         _transfer(address(this), msg.sender, amount);
     }
 
-    function multiLevelDistribution(address[] memory recipients, uint256[] memory amounts, uint8 tier) external onlyOwner {
+    function multiLevelDistribution(address[] memory recipients, uint256[] memory amounts, uint8 tier) external onlyAllowedUsers {
         require(recipients.length == amounts.length, "Arrays length mismatch");
 
         for (uint256 i = 0; i < recipients.length; i++) {
@@ -36,13 +37,13 @@ contract MyToken is ERC20, ERC20Permit {
         }
     }
     
-    function airDrop(address[] memory recipients, uint256 amount) external onlyOwner {
+    function airDrop(address[] memory recipients, uint256 amount) external onlyAllowedUsers {
         for (uint256 i = 0; i < recipients.length; i++) {
             _mint(recipients[i], amount);
         }
     }
 
-    function mintAndSend(address recipient, uint256 amount) external onlyOwner {
+    function mintAndSend(address recipient, uint256 amount) external onlyAllowedUsers {
         _mint(recipient, amount);
     }
 }
