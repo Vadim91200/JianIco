828f0899b0552e9e21f2d47554e56eda40e341f2
Vadim91200 <kamenets@hotmail.fr>
2023-10-22 22:07:03 +0200

    2nd version of the code, the contract is now verified on EtherScan and the tokens have been send the the teacher

--- src/JIAM.sol
+++ src/JIAM.sol
@@ -6,44 +6,48 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract JIAM is ERC20, ERC20Permit {
     mapping(address => bool) public allowedUsers;
-    mapping(address => uint8) public userTier;
+    enum Tier {One, Two, Three}
+    mapping(address => Tier) public userTiers;
 
     modifier onlyAllowedUsers() {
         require(allowedUsers[msg.sender], "User not allowed");
         _;
     }
     constructor() ERC20("Jian", "JAN") ERC20Permit("Jian") {
-        allowedUsers[msg.sender]= true;
+        allowedUsers[msg.sender] = true;
         _mint(msg.sender, 1000000 * 10 ** uint256(decimals()));
     }
 
-    function addToAllowlist(address user, uint8 tier) external onlyAllowedUsers {
+    function addToAllowlist(address user, Tier tier) external onlyAllowedUsers {
         allowedUsers[user] = true;
-        userTier[user] = tier;
+        userTiers[user] = tier;
     }
 
-    function getToken(uint256 amount) external payable {
+    function getToken(uint256 amount) external payable onlyAllowedUsers{
         require(msg.value >= amount, "Insufficient ETH sent");
         _mint(msg.sender, amount);
-        _transfer(address(this), msg.sender, amount);
     }
 
-    function multiLevelDistribution(address[] memory recipients, uint256[] memory amounts, uint8 tier) external onlyAllowedUsers {
-        require(recipients.length == amounts.length, "Arrays length mismatch");
-
+    function multiLevelDistribution(address[] calldata recipients) external payable onlyAllowedUsers {
         for (uint256 i = 0; i < recipients.length; i++) {
-            require(userTier[recipients[i]] == tier, "Recipient not in specified tier");
-            _mint(recipients[i], amounts[i]);
+            uint256 nbrOfTokensToMint;
+            Tier userTier = userTiers[recipients[i]];
+            if (userTier == Tier.One) {
+                nbrOfTokensToMint = msg.value * 1 * 10 ** uint256(decimals());
+            } else if (userTier == Tier.Two) {
+                nbrOfTokensToMint = msg.value * 2 * 10 ** uint256(decimals());
+            } else if (userTier == Tier.Three) {
+                nbrOfTokensToMint = msg.value * 3 * 10 ** uint256(decimals());
+            } else {
+                revert("You are not allowed to call this function.");
+            }
+            _mint(msg.sender, nbrOfTokensToMint);
         }
     }
     
-    function airDrop(address[] memory recipients, uint256 amount) external onlyAllowedUsers {
+    function airDrop(address[] calldata recipients, uint256 amount) external onlyAllowedUsers {
         for (uint256 i = 0; i < recipients.length; i++) {
             _mint(recipients[i], amount);
         }
-    }
-
-    function mintAndSend(address recipient, uint256 amount) external onlyAllowedUsers {
-        _mint(recipient, amount);
     }
 }
